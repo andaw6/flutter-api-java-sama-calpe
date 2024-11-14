4c4e1ebac51173a4fbcc3c647dbea5829a4074df
andaw6 <cissandaw@gmail.com>
2024-11-14 19:05:51 +0000

    modif image docker

--- src/main/java/com/ehacdev/flutter_api_java/web/dto/response/CreditPurchaseDTO.java
+++ src/main/java/com/ehacdev/flutter_api_java/web/dto/response/CreditPurchaseDTO.java
@@ -1,6 +1,6 @@
 package com.ehacdev.flutter_api_java.web.dto.response;
 
-import java.util.UUID;
+
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
--- src/main/java/com/ehacdev/flutter_api_java/web/dto/response/UserResponseDTO.java
+++ src/main/java/com/ehacdev/flutter_api_java/web/dto/response/UserResponseDTO.java
@@ -4,8 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.util.List;
-import java.util.Set;
+
 import java.util.UUID;
 
 import com.ehacdev.flutter_api_java.datas.enums.UserRole;
